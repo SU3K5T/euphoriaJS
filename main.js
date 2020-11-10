0e8473c91cb3f3a884533ba3dc078ad2321b0068
euphoria <surikat0013@icloud.com>
2020-11-10 23:54:30 +0300

    the third commit

--- main.js
+++ main.js
@@ -13,9 +13,25 @@ const a = prompt('Один из последних просмотренных ф
 	  c = prompt('Один из последних просмотренных фильмов?', ''),
 	  d = prompt('Насколько оцените его?', '');
 
+let secondPrivat = 12;
+
+
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
+personalMovieDB.privat = secondPrivat;
 console.log(personalMovieDB);
+
+console.log(personalMovieDB.privat);
+
+let age = 55;
+if (age >= 14 && age <= 90) {
+	console.log("true");
+	
+} else {
+	console.log('false');
+	
+}
+
 
 
 
